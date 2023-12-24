3247aa3a31a29ec88fb90e628df5fc19e2628960
hederson carrasquero <hederson_3_12@hotmail.com>
2023-12-24 11:38:40 -0500

    added geometry utils separation

--- Assets/CutMesh/ProceduralMesh.cs
+++ Assets/CutMesh/ProceduralMesh.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GeometryUtils;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
 {
@@ -144,60 +145,7 @@ public class ProceduralMesh : MonoBehaviour
                 _vertexBelow.Add(vertex);
         }
     }
-    private float GetSlope(Vector3 a, Vector3 b)
-    {
-        if (b.x - a.x == 0) return 0;
-        return (b.y - a.y / (b.x - a.x));
-    }
-    private Vector3 GetIntersection(Vector3 line1, Vector3 line2)
-    {
-        return Vector3.Cross(line1, line2);
-    }
-    private Vector3 GetLineOnGeneralForm(Vector3 P1, Vector3 P2)
-    {
-        return new Vector3(P2.y - P1.y,
-                                      P1.x - P2.x,
-                                      -(P1.x * P2.y) - (P2.x * P1.y));
-    }
-    private Vector3 FindCutPoint(Plane plane, Vector3 P1, Vector3 P2)
-    {
-        var lineA = GetLineOnGeneralForm(P1, P2);
-        Vector3 P3 = plane.ClosestPointOnPlane(P1);
-        Vector3 P4 = plane.ClosestPointOnPlane(P2);
-        var lineB = GetLineOnGeneralForm(P3, P4);
-        var intersectionPoint = GetIntersection(lineA, lineB);
-        var result = GetIntersection(P1, P2, P3, P4);
-        return result;
-
-
-    }
-    private Vector3 GetIntersection(Vector3 A, Vector3 B, Vector3 C, Vector3 D)
-    {
-        // Line AB represented as a1x + b1y = c1 
-        float a1 = B.y - A.y;
-        float b1 = A.x - B.x;
-        float c1 = a1 * (A.x) + b1 * (A.y);
-
-        // Line CD represented as a2x + b2y = c2 
-        float a2 = D.y - C.y;
-        float b2 = C.x - D.x;
-        float c2 = a2 * (C.x) + b2 * (C.y);
-
-        float determinant = a1 * b2 - a2 * b1;
-
-        if (determinant == 0)
-        {
-            // The lines are parallel. This is simplified 
-            // by returning a pair of FLT_MAX 
-            return C;
-        }
-        else
-        {
-            float x = (b2 * c1 - b1 * c2) / determinant;
-            float y = (a1 * c2 - a2 * c1) / determinant;
-            return new Vector3(x, y);
-        }
-    }
+   
     private bool VertexIsAbovePlane(Plane plane, Vector3 vertex)
     {
         var isAbove = plane.GetSide(vertex);
